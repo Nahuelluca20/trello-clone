911676527be58964a12a10df50bf8fad73ad9ac5
Nahuel Luca <nahuelluca20@gmail.com>
2024-01-09 11:28:35 -0300

    Refactor routes and queries

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,20 +7,8 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="h-full flex flex-col items-center pt-20 bg-slate-900">
-      <img src="/remix-logo-new@dark.png" width="402" height="149" />
+    <div className="h-full flex flex-col items-center pt-20 ">
       <div className="space-y-4 max-w-md text-lg text-slate-300">
-        <p>
-          This is a demo app to show off the features of Remix and teach them
-          through some videos we've published on{" "}
-          <a
-            href="https://www.youtube.com/watch?v=RTHzZVbTl6c&list=PLXoynULbYuED9b2k5LS44v9TQjfXifwNu&pp=gAQBiAQB"
-            className="underline"
-          >
-            YouTube
-          </a>
-          .
-        </p>
         <p>
           It's a recreation of the popular drag and drop interface in{" "}
           <a href="https://trello.com" className="underline">
@@ -45,6 +33,12 @@ export default function Index() {
           Login
         </Link>
       </div>
+      <Link
+        to="/Home"
+        className="text-xl mt-20 font-medium text-brand-aqua underline"
+      >
+        home
+      </Link>
     </div>
   );
 }
