ab9f901ae1be18f80e7eb54276fd505e5a0ec90d
Nahuel Luca <nahuelluca20@gmail.com>
2024-06-15 17:14:38 -0300

    chore: Refactor Prisma client initialization for better error handling and retry logic

--- app/db/prisma.server.ts
+++ app/db/prisma.server.ts
@@ -6,15 +6,37 @@ declare global {
   var db: PrismaClient | undefined;
 }
 
+const createPrismaClient = () => {
+  const client = new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
+  const connectWithRetry = async (retries = 5) => {
+    while (retries) {
+      try {
+        await client.$connect();
+        console.log("Connected to the database");
+        break;
+      } catch (error) {
+        retries -= 1;
+        console.error(`Retries left: ${retries}`, error);
+        if (retries === 0) {
+          console.error("Could not connect to the database");
+          throw new Error("Could not connect to the database");
+        }
+        await new Promise((res) => setTimeout(res, 5000));
+      }
+    }
+  };
+  connectWithRetry();
+  return client;
+};
+
 if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
-  db.$connect();
+  db = createPrismaClient();
 } else {
   if (!global.db) {
-    global.db = new PrismaClient();
-    global.db.$connect();
+    global.db = createPrismaClient();
   }
-
   db = global.db;
 }
 
